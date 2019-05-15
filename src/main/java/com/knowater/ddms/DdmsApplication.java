afdfb46b26f76c7676e71f5a5fd648ae24a78dd8
wangpf <wangpf@csdn.net>
2019-05-15 10:21:43 +0800

    init code

---
+++ src/main/java/com/knowater/ddms/DdmsApplication.java
@@ -0,0 +1,13 @@
+package com.knowater.ddms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdmsApplication.class, args);
+    }
+
+}
